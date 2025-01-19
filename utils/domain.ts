52fc776efaafb07b70534274effdb886f637f047
khanhthanhdev <thanhkt27507@gmail.com>
2025-01-19 20:41:06 +0700

    add domain

--- utils/domain.ts
+++ utils/domain.ts
@@ -1,10 +1,10 @@
 export const domain =
   process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
-    ? "https://reactai.vasarai.net"
+    ? "https://comai.vercel.app"
     : process.env.VERCEL_BRANCH_URL
       ? `https://${process.env.VERCEL_BRANCH_URL}`
       : process.env.NEXT_PUBLIC_VERCEL_URL
         ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
         : process.env.NEXT_PUBLIC_DEVELOPMENT_URL
           ? process.env.NEXT_PUBLIC_DEVELOPMENT_URL
-          : "https://reactai.vasarai.net";
+          : "https://comai.vercel.app";
